8da15438ab9264b937de20d7943740e0bce89a1c
JLoeve <jianglufull@foxmail.com>
2018-07-08 18:05:34 +0800

    将响应检查函数升级成装饰器

--- bili_kits/utils/response_check.py
+++ bili_kits/utils/response_check.py
@@ -1,3 +1,4 @@
+from functools import wraps
 
 class ResponseError(Exception):
     """响应错误"""
@@ -21,15 +22,25 @@ def check(response_body):
         raise ValueError('响应体中未找到code！')
     return obj['code'],obj['message'] if 'message' in obj else None
 
-def raise_check(response_body,ignore_codes=[],error_dict={}):
-    """如果未通过响应代码检查，则抛出响应异常
-    response_body:欲检查的响应体
-    ignore_codes:忽视的代码列表
-    error_dict:存放错误的字典
+def resp_check(ignore_codes=[],error_dict={},result_func=None):
+    """【装饰器】装饰的函数的返回响应体如果未通过响应代码检查，则抛出响应异常\n
+    response_body:欲检查的响应体\n
+    ignore_codes:忽视的代码列表\n
+    error_dict:存放错误的字典\n
+    result_func:对结果进行处理的函数，最终将返回该函数的返回结果，默认为`None`,将直接返回源结果
     """
-    code,msg=check(response_body)
-    if code and code not in ignore_codes:
-        if code in error_dict:
-            raise error_dict[code](code,msg)
-        raise ResponseError(code,msg)
-    
+    def des_resp(func):
+        @wraps(func)
+        def des(*args,**kwargs):
+            result=func(*args,**kwargs)
+            code,msg=check(result)
+            if code and code not in ignore_codes:
+                if code in error_dict:
+                    raise error_dict[code](code,msg)
+                raise ResponseError(code,msg)
+            if result_func:
+                return result_func(result)
+            return result
+        return des
+    return des_resp
+
